257b4179c591178f033544b3c0fda47f8cd148e0
Yueling Jiang <jyueling@bu.edu>
2019-03-28 18:16:39 -0400

    hw3

---
+++ HW3/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.get('/', function(req, res, next) {
+  request({
+        uri: 'https://aws.random.cat/meow'},
+      function (error,response,body) {
+        res.body = JSON.parse(body);
+        res.render('index', { title:'Meow',content: body});
+      });
+  //res.send('respond with a resource');
+
+});
+module.exports = router;
